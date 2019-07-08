95d1ec078b6324e8e1b7af7a72a9d6e8769b89d7
wool0826 <chanwool94@gmail.com>
2019-07-08 23:27:09 +0900

    foreground service
    j

--- app/src/main/java/com/example/memorize/LockService.java
+++ app/src/main/java/com/example/memorize/LockService.java
@@ -2,15 +2,20 @@ package com.example.memorize;
 
 import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 public class LockService extends Service{
 
@@ -35,6 +40,31 @@ public class LockService extends Service{
                 }
             }
         }
+
+        NotificationCompat.Builder builder;
+        String channelId = "Channel";
+        String channelName = "Channel Name";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+
+        NotificationManager notificationManager
+                = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel mChannel = new NotificationChannel(
+                    channelId, channelName, importance
+            );
+
+            notificationManager.createNotificationChannel(mChannel);
+            builder = new NotificationCompat.Builder(this, channelId);
+        } else {
+            builder = new NotificationCompat.Builder(this);
+        }
+
+        builder.setSmallIcon(R.drawable.ic_notifications_black_24dp);
+        builder.setContentTitle("Service Running..");
+
+        startForeground(1, builder.build());
+
         return START_REDELIVER_INTENT;
     }
 
